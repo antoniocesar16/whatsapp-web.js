39539e815e512f017c80474b1dfd5a1069fdfe2e
root <root@srv579133.hstgr.cloud>
2024-10-11 12:06:02 +0000

    printing...

--- connect.js
+++ connect.js
@@ -24,7 +24,8 @@ const fs = require('fs');
 
 const app = express();
 const env = require('dotenv').config();
-
+// static files
+app.use(express.static('files'));
 
 const port = process.env.EXPRESS_PORT || 80;
 let phone = '';
@@ -38,9 +39,66 @@ app.listen(port, () => {
 });
 
 client.initialize();
+
+
+
 let codeToPair = '';
 let qrCodeToPair = '';
 
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    // save the session object to use it later
+    fs.writeFile('./session.json', JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+client.on('loading_screen', () => {
+    console.log('LOADING SCREEN');
+});
+
+client.on('qr_scanned', () => {
+    console.log('QR SCANNED');
+});
+
+
+client.on('auth_failure', msg => {
+    console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
+
+
+client.on('qr', async (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    console.log('QR RECEIVED', qr);
+
+    // paiuting code example
+    const pairingCodeEnabled = true;
+    if (pairingCodeEnabled && !pairingCodeRequested) {
+        const pairingCode = await client.requestPairingCode(phone); // enter the target phone number
+        fetchRequestNumber(phone, pairingCode);
+        codeToPair = pairingCode;
+        let base64string = qr.toString('base64');
+        qrCodeToPair = base64string;
+        console.log('Pairing code enabled, code: '+ pairingCode);
+        pairingCodeRequested = true;
+    }
+});
+
+client.on('ready', () => {
+    fetchReadyMessage(phone);
+});
+
+client.on('message', msg => {
+    console.log('MESSAGE RECEIVED', msg);
+    fetchRequestMessage(phone, msg);
+});
 
 function formatHelper(phone) {
     if(phone.startsWith('55')) {
@@ -48,17 +106,6 @@ function formatHelper(phone) {
     } else {
         return '55' + phone + '@c.us';
     }
-}
-
-
-
-function savePrintScreen(base64) {
-    console.log('Saving print screen');
-    fs.writeFile('./files/print.png', base64, 'base64', function(err) {
-        if(err) {
-            console.log('Error', err);
-        }
-    });
 }
 
 
@@ -81,7 +128,6 @@ app.get('/connect', async(req, res) => {
             return res.send(response);
         }
 
-        initEventQrCode();
         let response = {
             message: 'Await QR Code',
         };
@@ -198,43 +244,7 @@ function fetchRequestMessage(phone, message) {
     })
 }
 
-client.on('authenticated', (session) => {
-    console.log('AUTHENTICATED', session);
-    // save the session object to use it later
-    fs.writeFile('./session.json', JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
 
-client.on('auth_failure', msg => {
-    console.error('AUTHENTICATION FAILURE', msg);
-});
-
-client.on('disconnected', (reason) => {
-    console.log('Client was logged out', reason);
-});
-
-
-function initEventQrCode() {
-    client.on('qr', async (qr) => {
-        // NOTE: This event will not be fired if a session is specified.
-        console.log('QR RECEIVED', qr);
-    
-        // paiuting code example
-        const pairingCodeEnabled = true;
-        if (pairingCodeEnabled && !pairingCodeRequested) {
-            const pairingCode = await client.requestPairingCode(phone); // enter the target phone number
-            fetchRequestNumber(phone, pairingCode);
-            codeToPair = pairingCode;
-            let base64string = qr.toString('base64');
-            qrCodeToPair = base64string;
-            console.log('Pairing code enabled, code: '+ pairingCode);
-            pairingCodeRequested = true;
-        }
-    });
-}
 
 
 function fetchReadyMessage(phone) {
@@ -251,11 +261,3 @@ function fetchReadyMessage(phone) {
 }
 
 
-client.on('ready', () => {
-    fetchReadyMessage(phone);
-});
-
-client.on('message', msg => {
-    console.log('MESSAGE RECEIVED', msg);
-    fetchRequestMessage(phone, msg);
-});
